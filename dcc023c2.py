e5675ad59781fabd19a65a652b16d4990e00123f
Priscila De-Stefano <priscila.destefano@gmail.com>
2018-04-25 14:39:01 -0300

    Adding timeout

--- dcc023c2.py
+++ dcc023c2.py
@@ -126,6 +126,7 @@ def splitTwoByTwo(val):
 def startClient(IP, PORT, INPUT, OUTPUT):
 	tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # criando socket)
 	tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 15)
+	tcp.settimeout(1) # timeout em segundos
 	# nao funciona no mac hihihihihi
 	# tcp.setsockopt(socket.SOL_SOCKET, socket.SO_RCVTIMEO, 15)
 
@@ -150,28 +151,32 @@ def startClient(IP, PORT, INPUT, OUTPUT):
 	next = True
 	#enviando a quantidade de quadros
 	tcp.send(s.pack(int(len(frames)))
-	
+
 	while next and count < len(frames):
 		sendFrameClient(frames[count])
 		next = False
-		
+
 		sync = "dcc023c2dcc023c2"
-		# Recebe o pacote de ack
-		texto = con.recv(8) 
-		if sync == texto:
-			length = decode16(con.recv(2))
-			chksum = decode16(con.recv(2))
-			ID = decode16(con.recv(1))
-			flags = con.recv(1)
-			dados = decode16(con.recv(int(length)))
-			frame = Frame(sync, length, chksum, ID, flags, dados)
-			msg = str(sync) + str(length) + str(0000)
-			msg += str(self.ID) + str(self.flags) + str(self.data)
-			result_check = checksum(msg)
-			# se receber o ack corretamente, envia o proximo frame
-			if result_check == chksum and length == 0 and flags = 0x80 and ID == frames[count].ID :
-				next = True
-				count+=count
+		try:
+			# Recebe o pacote de ack
+			texto = con.recv(8)
+			if sync == texto:
+				length = decode16(con.recv(2))
+				chksum = decode16(con.recv(2))
+				ID = decode16(con.recv(1))
+				flags = con.recv(1)
+				dados = decode16(con.recv(int(length)))
+				frame = Frame(sync, length, chksum, ID, flags, dados)
+				msg = str(sync) + str(length) + str(0000)
+				msg += str(self.ID) + str(self.flags) + str(self.data)
+				result_check = checksum(msg)
+				# se receber o ack corretamente, envia o proximo frame
+				if result_check == chksum and length == 0 and flags = 0x80 and ID == frames[count].ID :
+					next = True
+					count+=count
+		except socket.timeout:
+			 # se estourar timeout, reenvia frame
+			 print ("Reenviando frame ID")
 
 	tcp.close()
 
@@ -238,11 +243,11 @@ def handler(con, client, OUTPUT):
 	writeFile(OUTPUT, frames)
 
 def writeFile(output, frames):
-	file = open(output,"w") 
+	file = open(output,"w")
 	for frames in frame:
-		file.write(frame.data) 
+		file.write(frame.data)
 
-file.close() 
+file.close()
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
